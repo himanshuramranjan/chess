04d04c9dbf925e6aad389734dac48d659b50a48f
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-03-31 10:09:23 +0530

    Pieces defined for the board

---
+++ src/model/pieces/Queen.java
@@ -0,0 +1,18 @@
+package model.pieces;
+
+import enums.Color;
+
+public class Queen extends Piece {
+
+    public Queen(Color color, int row, int col) {
+        super(color, row, col);
+    }
+
+    @Override
+    public boolean isValidMove(int destRow, int destCol) {
+        super.isValidSelection(destRow, destCol);
+        int rowDiff = Math.abs(destRow - row);
+        int colDiff = Math.abs(destCol - col);
+        return (rowDiff == colDiff) || (row == destRow || col == destCol);
+    }
+}
