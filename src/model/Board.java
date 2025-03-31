89f060b66e8c2fcb50dc6cffc0013f7de9d2ca05
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-03-31 10:10:12 +0530

    Models defined for the chess

---
+++ src/model/Board.java
@@ -0,0 +1,73 @@
+package model;
+
+import enums.Color;
+import model.pieces.*;
+
+
+public class Board {
+
+    private static volatile Board chessBoard;
+    private static volatile Piece[][] board;
+
+    private Board() {
+        board = new Piece[8][8];
+        initializeBoard();
+    }
+
+    public static Board getInstance() {
+        if(board == null) {
+            synchronized (Board.class) {
+                if(board == null) {
+                    chessBoard = new Board();
+                }
+            }
+        }
+        return chessBoard;
+    }
+
+    private static void initializeBoard() {
+        // Initialize white pieces
+        board[0][0] = new Rook(Color.WHITE, 0, 0);
+        board[0][1] = new Knight(Color.WHITE, 0, 1);
+        board[0][2] = new Bishop(Color.WHITE, 0, 2);
+        board[0][3] = new Queen(Color.WHITE, 0, 3);
+        board[0][4] = new King(Color.WHITE, 0, 4);
+        board[0][5] = new Bishop(Color.WHITE, 0, 5);
+        board[0][6] = new Knight(Color.WHITE, 0, 6);
+        board[0][7] = new Rook(Color.WHITE, 0, 7);
+        for (int i = 0; i < 8; i++) {
+            board[1][i] = new Pawn(Color.WHITE, 1, i);
+        }
+
+        // Initialize black pieces
+        board[7][0] = new Rook(Color.BLACK, 7, 0);
+        board[7][1] = new Knight(Color.BLACK, 7, 1);
+        board[7][2] = new Bishop(Color.BLACK, 7, 2);
+        board[7][3] = new Queen(Color.BLACK, 7, 3);
+        board[7][4] = new King(Color.BLACK, 7, 4);
+        board[7][5] = new Bishop(Color.BLACK, 7, 5);
+        board[7][6] = new Knight(Color.BLACK, 7, 6);
+        board[7][7] = new Rook(Color.BLACK, 7, 7);
+        for (int i = 0; i < 8; i++) {
+            board[6][i] = new Pawn(Color.BLACK, 6, i);
+        }
+    }
+
+    public Piece getPiece(int row, int col) {
+        return board[row][col];
+    }
+
+    public static void setPiece(int row, int col, Piece piece) {
+        board[row][col] = piece;
+    }
+
+    public boolean isCheckmate(Color color) {
+        // TODO: Implement checkmate logic
+        return false;
+    }
+
+    public boolean isStalemate(Color color) {
+        // TODO: Implement stalemate logic
+        return false;
+    }
+}
---
+++ src/model/Player.java
@@ -0,0 +1,34 @@
+package model;
+
+
+import enums.Color;
+import exception.InvalidMoveException;
+import model.pieces.Piece;
+
+public class Player {
+    private final Color color;
+
+    public Player(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void makeMove(Move move) {
+        Piece piece = move.getPiece();
+        int destRow = move.getRow();
+        int destCol = move.getCol();
+
+        if (piece.isValidMove(destRow, destCol)) {
+            int sourceRow = piece.getRow();
+            int sourceCol = piece.getCol();
+            Board.setPiece(sourceRow, sourceCol, null);
+            Board.setPiece(destRow, destCol, piece);
+            piece.setPosition(destRow, destCol);
+        } else {
+            throw new InvalidMoveException("Invalid move!");
+        }
+    }
+}
